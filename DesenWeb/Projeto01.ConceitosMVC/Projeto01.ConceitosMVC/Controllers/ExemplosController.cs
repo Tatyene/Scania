b186b4b48f21187b04fb082c85bccf54da352ebd
Tatyene <tatyene0409@yahoo.com.br>
2022-12-20 00:04:41 -0300

    Desenvolvimento_Web

---
+++ DesenWeb/Projeto01.ConceitosMVC/Projeto01.ConceitosMVC/Controllers/ExemplosController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Projeto01.ConceitosMVC.Models;
+using System;
+
+namespace Projeto01.ConceitosMVC.Controllers
+{
+    public class ExemplosController : Controller
+    {
+        //action
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string ExibirTexto()
+        {
+            return "<h2>Retorno de uma string gerada pelo action.</h2>";
+        }
+        public IActionResult Conteudo()
+        {
+            return View();
+        }
+
+        //Views fracamente tipadas: Os dados são recebidos do formulário
+        //individualmente e processados no action
+        [HttpGet] // se não for declarado, toda requisição será Get!
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Cadastro(IFormCollection form)
+        {
+            try
+            {
+                Produto produto = new Produto()
+                {
+                    Codigo = int.Parse(form["txtCodigo"]),
+                    Descricao = form["txtDescricao"],
+                    Preco = double.Parse(form["txtPreco"])
+                };
+                return View("Resultado", produto);
+                //return RedirectToAction("Conteudo"); redireciona para outro Action manualmente.
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        // fortemente tipado. 
+        [HttpGet]
+        public IActionResult CadastroModel()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadastroModel(Produto produto)
+        {
+            return CadastroModelTag(produto);
+            //if (!ModelState.IsValid)
+            //{
+            //    return View();
+            //}
+
+            //return View("Resultado", produto);
+        }
+        [HttpGet]
+        public IActionResult CadastroModelTag() //Utilização de Tag Helper
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CadastroModelTag(Produto produto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            return View("Resultado", produto);
+        }
+    }
+}
---
+++ DesenWeb/Projeto01.ConceitosMVC/Projeto01.ConceitosMVC/Models/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Projeto01.ConceitosMVC.Models
+{
+    public class Utils
+    {
+        public static IEnumerable<Pessoa> ListarPessoas()
+        {
+            return new List<Pessoa>()
+            {
+                new Fornecedor()
+                {
+                    Cpf = "22991312807",
+                    Nome = "Ananias Silva",
+                    Email = "ananais@gmail.com",
+                    Telefone = "3258-2200"
+                },
+                new Cliente()
+                {
+                    Cpf = "2586994565",
+                    Nome = "Sebastião Golveia",
+                    Email = "sebastiao@gmail.com",
+                    Telefone = "2563-7854"
+                }
+            };
+        }
+    }
+}
